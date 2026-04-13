b53d1fd8a4fdd535d134ac46b4bd7f31330ff83d
Roman Zanko <roman.zanko.vl@gmail.com>
2026-04-13 10:15:57 +0400

    Extend `/query/execute` endpoint to accept request payload
    - Add `QueryRequest` struct with `metrics` and `dimensions` fields.
    - Update `execute_query` handler to deserialize JSON request body.

--- semantic-gateway-server/src/web_server/api/execute_query.rs
+++ semantic-gateway-server/src/web_server/api/execute_query.rs
@@ -1,7 +1,13 @@
-use actix_web::{HttpResponse, post};
+use actix_web::{HttpResponse, post, web};
 
 #[post("/query/execute")]
 #[tracing::instrument(name = "Execute query")]
-pub(crate) async fn execute_query() -> HttpResponse {
+pub(crate) async fn execute_query(request: web::Json<QueryRequest>) -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct QueryRequest {
+    metrics: Vec<String>,
+    dimensions: Vec<String>,
 }
